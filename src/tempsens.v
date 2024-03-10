b24c018b543e3e10be03eb85bec4eb3e7912868b
Harald Pretl <harald@pretl.eu>
2024-03-10 18:49:22 +0100

    Extend copyright with JKU

--- src/tempsens.v
+++ src/tempsens.v
@@ -1,4 +1,4 @@
-//	Copyright 2024 Harald Pretl
+//	Copyright 2024 Harald Pretl, IIC@JKU
 //
 //	Licensed under the Apache License, Version 2.0 (the "License");
 //	you may not use this file except in compliance with the License.
